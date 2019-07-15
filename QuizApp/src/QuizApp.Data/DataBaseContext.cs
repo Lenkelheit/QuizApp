249462965a1c9b29ad423770a6ca0c599a47b9d5
Nazariy Tymtsiv <tymtsiv2710@gmail.com>
2019-07-15 19:27:49 +0300

    Fix connectionString location, add constraints to strings and others

--- QuizApp/src/QuizApp.Data/DataBaseContext.cs
+++ QuizApp/src/QuizApp.Data/DataBaseContext.cs
@@ -5,71 +5,66 @@ namespace QuizApp.Data
 {
     public class DataBaseContext : DbContext
     {
-        // CONSTRUCTORS
-        public DataBaseContext()
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            Database.Migrate();
         }
 
-        // PROPERTIES
+
         public DbSet<User> Users { get; set; }
+
         public DbSet<Test> Tests { get; set; }
+
         public DbSet<Url> Urls { get; set; }
+
         public DbSet<TestResult> TestResults { get; set; }
+
         public DbSet<ResultAnswer> ResultAnswers { get; set; }
+
         public DbSet<ResultAnswerOption> ResultAnswerOptions { get; set; }
+
         public DbSet<TestQuestion> TestQuestions { get; set; }
+
         public DbSet<TestQuestionOption> TestQuestionOptions { get; set; }
 
-        // METHODS
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=QuizAppDB;Trusted_Connection=True;");
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Configure entities
 
-            // Configure User
-            modelBuilder.Entity<User>().Property(u => u.Username).IsRequired();
-            modelBuilder.Entity<User>().Property(u => u.Email).IsRequired();
-            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Username).IsRequired().HasMaxLength(EntitiesConstraints.USERNAME_MAX_LENGTH);
+            modelBuilder.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(EntitiesConstraints.EMAIL_MAX_LENGTH);
+            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired().HasMaxLength(EntitiesConstraints.PASSWORD_MAX_LENGTH);
             modelBuilder.Entity<User>().HasMany(u => u.CreatedTests).WithOne(t => t.Author).HasForeignKey(t => t.AuthorId);
 
-            // Configure Test
-            modelBuilder.Entity<Test>().Property(t => t.Title).IsRequired();
+            modelBuilder.Entity<Test>().Property(t => t.Title).IsRequired().HasMaxLength(EntitiesConstraints.TITLE_MAX_LENGTH);
+            modelBuilder.Entity<Test>().Property(t => t.Description).HasMaxLength(EntitiesConstraints.DESCRIPTION_MAX_LENGTH);
             modelBuilder.Entity<Test>().Property(t => t.TimeLimitSeconds).IsRequired().HasColumnType("time");
             modelBuilder.Entity<Test>().Property(t => t.LastModifiedDate).IsRequired().HasColumnType("datetime2");
             modelBuilder.Entity<Test>().HasMany(t => t.Urls).WithOne(u => u.Test).HasForeignKey(u => u.TestId);
             modelBuilder.Entity<Test>().HasMany(t => t.TestQuestions).WithOne(q => q.Test).HasForeignKey(q => q.TestId);
 
-            // Configure Url
             modelBuilder.Entity<Url>().Property(u => u.ValidFromTime).IsRequired().HasColumnType("datetime2");
             modelBuilder.Entity<Url>().Property(u => u.ValidUntilTime).IsRequired().HasColumnType("datetime2");
             modelBuilder.Entity<Url>().HasMany(u => u.TestResults).WithOne(tr => tr.Url).HasForeignKey(tr => tr.UrlId);
 
-            // Configure TestResult
-            modelBuilder.Entity<TestResult>().Property(tr => tr.IntervieweeName).IsRequired();
+            modelBuilder.Entity<TestResult>().Property(tr => tr.IntervieweeName).IsRequired().HasMaxLength(EntitiesConstraints.INTERVIEWEE_NAME_MAX_LENGTH);
             modelBuilder.Entity<TestResult>().Property(tr => tr.PassingStartTime).IsRequired().HasColumnType("datetime2");
             modelBuilder.Entity<TestResult>().Property(tr => tr.PassingEndTime).IsRequired().HasColumnType("datetime2");
             modelBuilder.Entity<TestResult>().HasMany(tr => tr.ResultAnswers).WithOne(ra => ra.Result).HasForeignKey(ra => ra.ResultId);
 
-            // Configure ResultAnswer
             modelBuilder.Entity<ResultAnswer>().Property(ra => ra.TimeTakenSeconds).IsRequired().HasColumnType("time");
             modelBuilder.Entity<ResultAnswer>().HasOne(ra => ra.Question).WithMany(q => q.ResultAnswers).HasForeignKey(ra => ra.QuestionId).OnDelete(DeleteBehavior.SetNull);
             modelBuilder.Entity<ResultAnswer>().HasMany(ra => ra.ResultAnswerOptions).WithOne(raopt => raopt.ResultAnswer).HasForeignKey(raopt => raopt.ResultAnswerId);
 
-            // Configure ResultAnswerOption
             modelBuilder.Entity<ResultAnswerOption>().HasOne(raopt => raopt.Option).WithMany(tqopt => tqopt.ResultAnswerOptions).HasForeignKey(raopt => raopt.OptionId).OnDelete(DeleteBehavior.SetNull);
 
-            // Configure TestQuestion
-            modelBuilder.Entity<TestQuestion>().Property(tq => tq.Text).IsRequired();
+            modelBuilder.Entity<TestQuestion>().Property(tq => tq.Text).IsRequired().HasMaxLength(EntitiesConstraints.TEXT_QUESTION_MAX_LENGTH);
+            modelBuilder.Entity<TestQuestion>().Property(tq => tq.Hint).HasMaxLength(EntitiesConstraints.HINT_MAX_LENGTH);
             modelBuilder.Entity<TestQuestion>().Property(tq => tq.TimeLimitSeconds).IsRequired().HasColumnType("time");
             modelBuilder.Entity<TestQuestion>().HasMany(tq => tq.TestQuestionOptions).WithOne(tqopt => tqopt.Question).HasForeignKey(tqopt => tqopt.QuestionId);
 
-            // Configure TestQuestionOption
-            modelBuilder.Entity<TestQuestionOption>().Property(tqopt => tqopt.Text).IsRequired();
+            modelBuilder.Entity<TestQuestionOption>().Property(tqopt => tqopt.Text).IsRequired().HasMaxLength(EntitiesConstraints.TEXT_QUESTION_OPTION_MAX_LENGTH);
 
             base.OnModelCreating(modelBuilder);
         }
--- QuizApp/src/QuizApp.Entities/Test.cs
+++ QuizApp/src/QuizApp.Entities/Test.cs
@@ -6,14 +6,22 @@ namespace QuizApp.Entities
     public class Test
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
+
         public string Description { get; set; }
+
         public TimeSpan TimeLimitSeconds { get; set; }
+
         public DateTime LastModifiedDate { get; set; }
+
         public int AuthorId { get; set; }
 
+
         public User Author { get; set; }
+
         public ICollection<Url> Urls { get; set; } = new List<Url>();
+
         public ICollection<TestQuestion> TestQuestions { get; set; } = new List<TestQuestion>();
     }
 }
