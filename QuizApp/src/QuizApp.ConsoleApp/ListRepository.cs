4459018c7dbc845fcbb1096dda398ee987ee1789
Nazariy Tymtsiv <tymtsiv2710@gmail.com>
2019-07-26 13:41:51 +0300

    Add 'ListRepository' class and tests to it

---
+++ QuizApp/src/QuizApp.ConsoleApp/ListRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+
+using QuizApp.Data.Interfaces;
+
+namespace QuizApp.ConsoleApp
+{
+    public class ListRepository<T> : IRepository<T> where T : class
+    {
+        public List<T> List { get; } = new List<T>();
+
+        public int Count(Expression<Func<T, bool>> predicate = null)
+        {
+            return predicate == null ? List.Count : List.Count(predicate.Compile());
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> filter = null,
+                                Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                Func<IQueryable<T>, IIncludableQueryable<T, object>> includeProperties = null)
+        {
+            IQueryable<T> query = List.AsQueryable();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (orderBy != null) query = orderBy(query);
+
+            return query;
+        }
+
+        public T GetById(object id, Func<IQueryable<T>, IIncludableQueryable<T, object>> includeProperties = null)
+        {
+            return List[(int)id];
+        }
+
+        public async Task<T> GetByIdAsync(object id, Func<IQueryable<T>, IIncludableQueryable<T, object>> includeProperties = null)
+        {
+            return await Task.Run(() => List[(int)id]);
+        }
+
+        public void Insert(T entity)
+        {
+            List.Add(entity);
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await Task.Run(() => List.Add(entity));
+        }
+
+        public void Update(T entityToUpdate)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Delete(T entityToDelete)
+        {
+            List.Remove(entityToDelete);
+        }
+
+        public void Delete(Expression<Func<T, bool>> predicate)
+        {
+            if (predicate != null) List.RemoveAll(elem => predicate.Compile().Invoke(elem));
+            else List.Clear();
+        }
+    }
+}
---
+++ QuizApp/src/QuizApp.ConsoleApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace QuizApp.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ListRepository<string> listRepository = new ListRepository<string>();
+
+            string[] strArr = { "Hello", "World", "Lorem", "Ipsum", "No", "Time", "Monday" };
+
+            // insert sync and async
+            Console.WriteLine("insert sync and async");
+            listRepository.Insert(strArr[0]);
+            await listRepository.InsertAsync(strArr[1]);
+            Show(listRepository.List);
+
+            // delete one element
+            Console.WriteLine("delete one element");
+            listRepository.Delete(strArr[0]);
+            Show(listRepository.List);
+
+            // delete non existent element
+            Console.WriteLine("delete non existent element");
+            listRepository.Delete(strArr[4]);
+            Show(listRepository.List);
+
+            // delete all elements, add new ones, delete elements by some predicate
+            Console.WriteLine("delete all elements, add new ones, delete elements by some predicate");
+            listRepository.Delete(null as Expression<Func<string, bool>>);
+            listRepository.Insert(strArr[0]);
+            listRepository.Insert(strArr[1]);
+            listRepository.Insert(strArr[2]);
+            listRepository.Insert(strArr[3]);
+            listRepository.Insert(strArr[4]);
+            Show(listRepository.List);
+            listRepository.Delete(str => str.Contains("o"));
+            Show(listRepository.List);
+
+            // get by id sync and async
+            Console.WriteLine("get by id sync and async");
+            listRepository.Delete(null as Expression<Func<string, bool>>);
+            listRepository.Insert(strArr[0]);
+            listRepository.Insert(strArr[1]);
+            listRepository.Insert(strArr[2]);
+            Show(listRepository.List);
+            Console.WriteLine(listRepository.GetById(1));
+            Console.WriteLine(await listRepository.GetByIdAsync(2));
+
+            // count all elements and by predicate
+            Console.WriteLine($"{Environment.NewLine}count all elements and by predicate");
+            listRepository.Delete(null as Expression<Func<string, bool>>);
+            listRepository.Insert(strArr[0]);
+            listRepository.Insert(strArr[1]);
+            listRepository.Insert(strArr[2]);
+            Show(listRepository.List);
+            Console.WriteLine(listRepository.Count());
+            Console.WriteLine(listRepository.Count(str => str.Contains("e")));
+
+            // get all elements and by predicate, sort all elements, filter elements and then sort them
+            Console.WriteLine($"{Environment.NewLine}get all elements and by predicate, sort all elements, filter elements and then sort them");
+            listRepository.Delete(null as Expression<Func<string, bool>>);
+            listRepository.Insert(strArr[0]);
+            listRepository.Insert(strArr[1]);
+            listRepository.Insert(strArr[2]);
+            Show(listRepository.List);
+            Show(listRepository.Get());
+            Show(listRepository.Get(filter: str => str.Contains("e")));
+            Show(listRepository.Get(orderBy: str => str.OrderBy(s => s)));
+            Show(listRepository.Get(filter: str => str.Contains("e"), orderBy: str => str.OrderByDescending(s => s)));
+
+            Console.ReadLine();
+        }
+
+        static void Show(IEnumerable<string> list)
+        {
+            foreach (string str in list) 
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+        }
+    }
+}
